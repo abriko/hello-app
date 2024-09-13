3dab3cf203cf564b600a054968104fcf39f2456c
HuPo <i@hupo.me>
2024-09-13 07:59:55 +0000

    frist commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import random
+import sys
+from flask import Flask
+
+ERROR_RATE = 90
+
+app = Flask(__name__)
+
+def isError(percentage):
+	return random.randint(1, 100) <= percentage
+
+@app.route('/')
+def hello():
+	return "Hello World!"
+
+@app.route('/probe')
+def probe():
+	return "OK"
+
+@app.route('/error')
+def error():
+	if isError(ERROR_RATE):
+		return "Error found!", 500
+	else:
+		return "No error found!"
+
+if __name__ == '__main__':
+	if len(sys.argv) >= 2:
+		ERROR_RATE = int(sys.argv[1])
+	
+	print(f"Error rate is {ERROR_RATE}%")
+	app.run(host='0.0.0.0', port=8080)
